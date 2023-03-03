ae84b0be1bfc0a58f487cdbabe28934e5ab29e8b
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-03-02 21:44:30 -0500

    Add function

--- python/call_pyq.py
+++ python/call_pyq.py
@@ -10,6 +10,9 @@ def t_bin_op(elem, k_tail):
     rhs = transpile(k_tail.pop(), k_tail)
     return BinOp(lhs, Add(), rhs)
 
+def t_func(elem, k_tail):
+    return FunctionDef(str(elem), [])
+    #return Constant(str(elem))
 
 def transpile(elem, k_tail):
     type = q.type(elem)
@@ -18,6 +21,8 @@ def transpile(elem, k_tail):
         out = t_bin_op(elem, k_tail)
     elif str_type == '-7h':
         out = t_int(elem, k_tail)
+    elif str_type == '-11h':
+        out = t_func(elem, k_tail)
     else:
         raise NotImplementedError
     return out
@@ -27,12 +32,19 @@ def parse(input_q_code):
     text = 'parse \"{}\"'.format(input_q_code)
     return list(q(text))
 
-def main():
-    print("Hello World!")
-    a = parse("1+2")
+def parse_and_transpile(input_q_code):
+    a = parse(input_q_code)
     a.reverse()
     elem = a.pop()
     b = transpile(elem, a)
+    return b
+
+def main():
+    print("Hello World!")
+    a = parse_and_transpile("1+2")
+    print(unparse(a))
+    #b = parse_and_transpile("factorial:{$[x<2;1;x*.z.s x-1]}")
+    b = parse_and_transpile("f:{2*x;}")
     print(unparse(b))
 
 
