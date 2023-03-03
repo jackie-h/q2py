2a6042b06c75013fbe73400cdf3009f5677540ed
jackie-h <10471950+jackie-h@users.noreply.github.com>
2023-03-02 23:51:13 -0500

    Start to parse a function

--- python/q_treesitter.py
+++ python/q_treesitter.py
@@ -1,4 +1,4 @@
-from _ast import Add, BinOp, Constant
+from _ast import Add, BinOp, Constant, FunctionDef
 from ast import unparse
 
 from tree_sitter import Language, Parser, Node
@@ -12,9 +12,17 @@ def convert_expr_list(node: Node, tail, out: deque):
         transpile(node, l, out)
 
 
+def convert_expr_seq(node: Node, tail, out: deque):
+    l = list(node.children)
+    while len(l) > 0:
+        node = l.pop()
+        transpile(node, l, out)
+
 def convert_number(node: Node, tail, out: deque):
     out.append(Constant(float(node.text.decode('utf-8'))))
 
+def convert_string(node: Node, tail, out: deque):
+    out.append(Constant(node.text.decode('utf-8')))
 
 def convert_operator(node: Node, tail, out: deque):
     lhs = out.pop()
@@ -25,6 +33,34 @@ def convert_operator(node: Node, tail, out: deque):
         out.append(op)
 
 
+def convert_local_var(node: Node, tail, out: deque):
+    assert node.child_count == 3
+    var_name = node.children[0].text.decode('utf-8')
+    transpile(node.children[2], tail, out)
+    value = out.pop()
+    if node.children[2].type == "function_body":
+        func = FunctionDef(var_name, [], body=[value],
+                           decorator_list=[],lineno=0)
+        out.append(func)
+    print(node)
+
+
+def convert_function_body(node: Node, tail, out: deque):
+    assert node.child_count == 3
+    transpile(node.children[1], tail, out)
+    print(node)
+def convert_namespace(node: Node, tail, out: deque):
+    assert node.child_count == 2
+    convert_string(node.children[1], tail, out)
+
+def convert_entity_name(node: Node, tail, out: deque):
+    assert node.child_count == 0
+    convert_string(node, tail, out)
+
+def convert_table(node: Node, tail, out: deque):
+    assert node.child_count == 3
+    transpile(node.children[1], tail, out)
+
 def  transpile(node: Node, tail, out: deque):
     print(node.type)
     if node.type == "source_file":
@@ -32,10 +68,28 @@ def  transpile(node: Node, tail, out: deque):
             transpile(child, [], out)
     elif node.type == "expr_list":
         convert_expr_list(node, tail, out)
+    elif node.type == "expr_seq":
+        convert_expr_seq(node, tail, out)
     elif node.type == "number":
         convert_number(node, [], out)
+    elif node.type == "string":
+        convert_string(node, [], out)
     elif node.type == "operator":
         convert_operator(node, tail, out)
+    elif node.type == "local_variable_assignment":
+        convert_local_var(node, tail, out)
+    elif node.type == "function_body":
+        convert_function_body(node, tail, out)
+    elif node.type == "namespace":
+        convert_namespace(node, tail, out)
+    elif node.type == "entity_name":
+        convert_entity_name(node, tail, out)
+    elif node.type == "table":
+        convert_table(node, tail, out)
+    elif node.type == ";":
+        None
+    elif node.type == ":":
+        None
     else:
         raise NotImplementedError
     return out
@@ -60,7 +114,9 @@ def main():
     parser.set_language(Q_LANGUAGE)
 
     tree = parser.parse(bytes("""
-        1+2
+    .test.test_add:{
+      AEQ[3;1+2;""]
+    };
         """, "utf8"))
 
     root_node = tree.root_node
